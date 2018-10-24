d2244ba927b1b8199c90bbd7bea673900a51bd74
Jordan Halterman <jordan.halterman@gmail.com>
2018-10-24 12:16:29 -0700

    Update endpoints.

--- atomix/primitives.py
+++ atomix/primitives.py
@@ -11,19 +11,19 @@ class Map(Primitive):
         super(Map, self).__init__(client, name)
 
     def get(self, key):
-        return self.client.get('/v1/primitives/maps/{name}/{key}', name=self.name, key=key)
+        return self.client.get('/v1/atomic-map/{name}/{key}', name=self.name, key=key)
 
     def put(self, key, value):
-        return self.client.put('/v1/primitives/maps/{name}/{key}', name=self.name, key=key, value=value, headers={'content-type': 'text/plain'})
+        return self.client.put('/v1/atomic-map/{name}/{key}', name=self.name, key=key, data=value, headers={'content-type': 'text/plain'})
 
     def remove(self, key):
-        return self.client.delete('/v1/primitives/maps/{name}/{key}', name=self.name, key=key)
+        return self.client.delete('/v1/atomic-map/{name}/{key}', name=self.name, key=key)
 
     def size(self):
-        return self.client.get('/v1/primitives/maps/{name}/size', name=self.name)
+        return self.client.get('/v1/atomic-map/{name}/size', name=self.name)
 
     def clear(self):
-        return self.client.delete('/v1/primitives/maps/{name}', name=self.name)
+        return self.client.delete('/v1/atomic-map/{name}', name=self.name)
 
 
 class Set(Primitive):
@@ -32,19 +32,19 @@ class Set(Primitive):
         super(Set, self).__init__(client, name)
 
     def add(self, item):
-        return self.client.put('/v1/primitives/sets/{name}/{value}', name=self.name, value=item)
+        return self.client.put('/v1/set/{name}/{value}', name=self.name, value=item)
 
     def remove(self, item):
-        return self.client.delete('/v1/primitives/sets/{name}/{value}', name=self.name, value=item)
+        return self.client.delete('/v1/set/{name}/{value}', name=self.name, value=item)
 
     def contains(self, item):
-        return self.client.delete('/v1/primitives/sets/{name}/{value}', name=self.name, value=item)
+        return self.client.delete('/v1/set/{name}/{value}', name=self.name, value=item)
 
     def size(self):
-        return self.client.get('/v1/primitives/sets/{name}/size', name=self.name)
+        return self.client.get('/v1/set/{name}/size', name=self.name)
 
     def clear(self):
-        return self.client.delete('/v1/primitives/sets/{name}', name=self.name)
+        return self.client.delete('/v1/set/{name}', name=self.name)
 
 
 class Value(Primitive):
@@ -53,13 +53,13 @@ class Value(Primitive):
         super(Value, self).__init__(client, name)
 
     def get(self):
-        return self.client.get('/v1/primitives/values/{name}', name=self.name)
+        return self.client.get('/v1/atomic-value/{name}', name=self.name)
 
     def set(self, value):
-        return self.client.put('/v1/primitives/values/{name}', name=self.name, data=value, headers={'content-type': 'text/plain'})
+        return self.client.put('/v1/atomic-value/{name}', name=self.name, data=value, headers={'content-type': 'text/plain'})
 
     def compare_and_set(self, expect, update):
-        return self.client.post('/v1/primitives/values/{name}/cas', name=self.name, data={'expect': expect, 'update': update}, headers={'content-type': 'application/json'})
+        return self.client.post('/v1/atomic-value/{name}/cas', name=self.name, data={'expect': expect, 'update': update}, headers={'content-type': 'application/json'})
 
 
 class Counter(Primitive):
@@ -68,13 +68,13 @@ class Counter(Primitive):
         super(Counter, self).__init__(client, name)
 
     def get(self):
-        return self.client.get('/v1/primitives/counters/{name}', name=self.name)
+        return self.client.get('/v1/atomic-counter/{name}', name=self.name)
 
     def set(self, value):
-        return self.client.put('/v1/primitives/counters/{name}', name=self.name, data=value, headers={'content-type': 'text/plain'})
+        return self.client.put('/v1/atomic-counter/{name}', name=self.name, data=value, headers={'content-type': 'text/plain'})
 
     def increment(self):
-        return self.client.post('/v1/primitives/counters/{name}/inc', name=self.name)
+        return self.client.post('/v1/atomic-counter/{name}/inc', name=self.name)
 
 
 class Lock(Primitive):
@@ -83,7 +83,7 @@ class Lock(Primitive):
         super(Lock, self).__init__(client, name)
 
     def lock(self):
-        return self.client.post('/v1/primitives/locks/{name}', name=self.name)
+        return self.client.post('/v1/atomic-lock/{name}', name=self.name)
 
     def unlock(self):
-        return self.client.delete('/v1/primitives/locks/{name}', name=self.name)
+        return self.client.delete('/v1/atomic-lock/{name}', name=self.name)
